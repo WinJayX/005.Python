e9c53423789c1888dbce174bc44401b929cfdaea
WinJay <WinJayX@Gmail.com>
2020-11-05 01:36:18 +0800

    + #0; 新增练习文件.

---
+++ 02Day/02Day_打印三角形.py
@@ -0,0 +1,5 @@
+for i in range(80):
+    if i <= 40:
+        print(i * "*")
+    else:
+        print((80 - i) * "*")
---
+++ 02Day/Day02_打印车牌.py
@@ -0,0 +1,6 @@
+import random
+import string
+count = 0
+while count < 3:
+    print()
+random.choice(string.ascii_letters + string.digits)
