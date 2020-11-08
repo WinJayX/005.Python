59750f6076925ef37fa4a708aa2d8c9f24697f2d
WinJay <WinJayX@Gmail.com>
2020-11-09 00:09:48 +0800

    + #0; 新增练习文件.

--- 02Day/Day02_打印乘法表.py
+++ 02Day/Day02_打印乘法表.py
@@ -3,3 +3,15 @@ for i in range(1,10):               #从1开始取值，共取到9
     print()                         #print 参数自带换行属性，在此是为了这个外部大循环可以换行
     for j in range(1,i+1):          #从1开始取值，取到i，即是i+1
         print(f"{j} x {i} = {j * i}", end="   ") #end="",等于空，即不换行了，就是让内循环的都在一行上。
+
+print("")
+print(500*'=')
+
+
+for i in range(10):
+    if i < 10:
+        # print(f"1 * {i} ={1*i}")
+        print("")  # 打印换行
+        for j in range(1,i+1):      # 变量j 从1 开始，到 i结束，因为i+1了。range顾头不顾尾。
+            print(f"{j} * {i} = {j*i}", end="  ")
+
--- 02Day/Day02_打印车牌.py
+++ 02Day/Day02_打印车牌.py
@@ -1,6 +1,35 @@
 import random
 import string
+
+
 count = 0
 while count < 3:
-    print()
-random.choice(string.ascii_letters + string.digits)
+    # print("这个是取给定字符串中的一个字符--------------"+random.choice('dasfasdfasfas'))
+    # # print("========在给定的字符串中随机打印一个字符=======")
+    # count += 1
+    # print("这个是取所有字母及数字中的一个字符----------"+random.choice(string.ascii_letters + string.digits))
+    # # print("这个是取1--9 之间的随机数字----------" + random.randint(1, 9))
+    #
+
+
+
+    for i in range(20):
+        upp = random.choice(string.ascii_uppercase)         #京后面的字母
+        num = "".join(random.sample(string.ascii_uppercase + string.digits, 5))
+        s = (f'京{upp}*{num}')
+        print(s)
+    count += 1
+    number = input( "请输入您选择的车牌：" )
+    if number in s:
+
+        print( "恭喜您选中车牌" )
+    # elif:
+        print( "请再选一次" )
+        exit( "已经没有机会了哦。。" )
+
+
+
+
+
+
+
