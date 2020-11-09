c77722a0dca2b1a4ae4f23e98337519591750405
WinJayX <WinJayX@Gmail.com>
2020-11-09 22:21:55 +0800

    ^ #0; 修改文件内容，新增相应信息。

--- 02Day/Day02_打印车牌.py
+++ 02Day/Day02_打印车牌.py
@@ -4,6 +4,7 @@ import string
 
 count = 0
 while count < 3:
+    count += 1
     # print("这个是取给定字符串中的一个字符--------------"+random.choice('dasfasdfasfas'))
     # # print("========在给定的字符串中随机打印一个字符=======")
     # count += 1
@@ -11,21 +12,21 @@ while count < 3:
     # # print("这个是取1--9 之间的随机数字----------" + random.randint(1, 9))
     #
 
-
-
     for i in range(20):
         upp = random.choice(string.ascii_uppercase)         #京后面的字母
         num = "".join(random.sample(string.ascii_uppercase + string.digits, 5))
         s = (f'京{upp}*{num}')
-        print(s)
-    count += 1
+        print(s, end=" ")
+    print("")
     number = input( "请输入您选择的车牌：" )
-    if number in s:
+    if number == s:
 
         print( "恭喜您选中车牌" )
-    # elif:
+    elif number not in s:
         print( "请再选一次" )
-        exit( "已经没有机会了哦。。" )
+    else:
+        print( "已经没有机会了哦。。" )
+        break
 
 
 
