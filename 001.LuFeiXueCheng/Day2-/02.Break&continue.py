79397eee33536c1e0139f486cef89d48f50fec22
WinJay <WinJayX@Gmail.com>
2021-11-23 13:58:31 +0800

    ^ #0; Update Files.

--- 001.LuFeiXueCheng/Day2-/02.Break&continue.py
+++ 001.LuFeiXueCheng/Day2-/02.Break&continue.py
@@ -1,1 +1,5 @@
-for floor in range(1, 7):    print(f'当前是在第{floor}层'.center(50, '-'))    for room in range(1, 10):        print(f'当前是在{floor}0{room}室内')
+for floor in range(1, 7):
+    print(f'当前是在第{floor}层'.center(50, '-'))
+    for room in range(1, 10):
+        print(f'当前是在{floor}0{room}室内')
+        
