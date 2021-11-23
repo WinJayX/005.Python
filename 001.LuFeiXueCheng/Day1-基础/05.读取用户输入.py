cd2c3ba8d8825b7504243222c71e9360fa05d4b1
WinJay <WinJayX@Gmail.com>
2021-11-23 13:57:27 +0800

    ^ #0; Update Files.

--- 001.LuFeiXueCheng/Day1-基础/05.读取用户输入.py
+++ 001.LuFeiXueCheng/Day1-基础/05.读取用户输入.py
@@ -1,1 +1,20 @@
-# inputname = input("Your name is :  ").strip()age = int(input('Your age is :  '))Hobbie = input('You Hobbie is : ')info = f'''--------{name} Info---------Name: {name}Age:  {age}， wow still young , in {30 - age} years you will be 30 years olds.Hobbie: {Hobbie}-----------END INFO-----------'''print(info.strip())# strip() 函数，这个测试失败，没有去除空格
+# input
+
+name = input("Your name is :  ").strip()
+age = int(input('Your age is :  '))
+
+
+Hobbie = input('You Hobbie is : ')
+
+info = f'''
+--------{name} Info---------
+Name: {name}
+Age:  {age}， wow still young , in {30 - age} years you will be 30 years olds.
+Hobbie: {Hobbie}
+-----------END INFO-----------
+'''
+print(info.strip())
+
+# strip() 函数，这个测试失败，没有去除空格
+
+
