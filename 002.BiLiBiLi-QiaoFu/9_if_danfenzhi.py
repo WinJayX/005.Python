3cea590e70dfb66a658b8bb3d18d9706ff916f4f
WinJay <WinJayX@Gmail.com>
2021-11-23 13:59:41 +0800

    ^ #0; Update Files.

--- 002.BiLiBiLi-QiaoFu/9_if_danfenzhi.py
+++ 002.BiLiBiLi-QiaoFu/9_if_danfenzhi.py
@@ -1,1 +1,20 @@
-# 当你的工资大于3000时去烧烤，否则回家money = int(input("请输入你的工资："))if money > 8000:    print("把你小店给盘了！！我来当老板")elif money > 7000:    print("来个烤全羊🐑")elif money > 5000:    print("来个大烤鸭！")elif money > 3000:    print("来10串大腰子！！")    print("再来一瓶雪花酒！！")else:    print("快回家吧。。。")print("退出回家")
+# 当你的工资大于3000时去烧烤，否则回家
+money = int(input("请输入你的工资："))
+
+if money > 8000:
+    print("把你小店给盘了！！我来当老板")
+
+elif money > 7000:
+    print("来个烤全羊🐑")
+
+elif money > 5000:
+    print("来个大烤鸭！")
+
+elif money > 3000:
+    print("来10串大腰子！！")
+    print("再来一瓶雪花酒！！")
+
+else:
+    print("快回家吧。。。")
+
+print("退出回家")
